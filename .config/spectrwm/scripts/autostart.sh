353997fca3d78daa7cc45761ba47ef37b5d9c0d7
Brad Heffernan <ph3onix83@gmail.com>
2020-09-05 16:33:54 +0930

    updated

---
+++ .config/spectrwm/scripts/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+wal -nRa 90 &
+run conky -c $HOME/.conkyrc &
+run mpd &
+run glava &
+run dunst &
